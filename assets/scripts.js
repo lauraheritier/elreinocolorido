c8b7dede68c8c245dab13521a378643f5497734d
lauraheritiertrenders <150356588+lauraheritiertrenders@users.noreply.github.com>
2025-03-31 15:45:21 -0300

    ajustes

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+});
